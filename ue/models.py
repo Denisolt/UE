79e5a64e8a27a814025218644fd211ab39e6bf9a
Denisolt <dshakhbu@nyit.edu>
2017-01-06 17:03:17 -0500

    calendar

--- ue/models.py
+++ ue/models.py
@@ -1,10 +1,18 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
+from django.utils import timezone
 # Create your models here.
+# Mahmoud Saleh, [03.01.17 22:09]
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    date = models.DateTimeField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
     desc = models.TextField()
-    hexcolor = models.CharField(max_length=5)
+    hex = models.CharField(max_length=5)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- ue/views.py
+++ ue/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
-def index(request):
-    return render(request, 'ue/index.html', {})
 
-    
+
+def index(request):
+    events = Event.objects.filter().order_by('start')
+    return render(request, 'index.html', {'events': events})
